9b537f9b56cf550b8d1b745d6b3af55dcbb89357
Daniel <daniel.hakanson@bravomedia.se>
2017-12-02 19:21:54 +0100

    day 2 second challenge + spreadsheets

--- day2/main.py
+++ day2/main.py
@@ -9,8 +9,26 @@ def calc_spreadsheet(filename):
         sum = sum + (int(max(line_values)) - int(min(line_values)))
     print(sum)
 
+
+def calc_common_spreadsheet(filename):
+    sum = 0
+    for line in filename:
+        line_values = (re.findall('\d+', line))
+        line_values = map(int, line_values)
+        for v in line_values:
+            for c in line_values:
+                if(c != v and v%c == 0):
+                    sum = sum + (v/c)
+    print(sum)
+
 filename = open('spreadsheet.txt','r')
 calc_spreadsheet(filename)
 
 filename = open('spreadsheet2.txt', 'r')
 calc_spreadsheet(filename)
+
+filename = open('spreadsheet3.txt', 'r')
+calc_common_spreadsheet(filename)
+
+filename = open('spreadsheet2.txt', 'r')
+calc_common_spreadsheet(filename)
