468f621b2bbea6fdf42bcf0708d1f3ebad30d218
Daniel Håkanson <danielhk39@gmail.com>
2017-12-04 19:49:58 +0100

    day4, second challenge

--- day4/Main.java
+++ day4/Main.java
@@ -2,22 +2,26 @@ import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String [] args) {
 
         String fileName = "passwords.txt";
         String line = null;
-        int countValidPasswords = 0;
+		int countValidPasswords = 0;
+		int countSecurePasswords = 0;
         try {
             FileReader fileReader = new FileReader(fileName);
             // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             while((line = bufferedReader.readLine()) != null) {
-                countValidPasswords += validatePassword(line);
+				countValidPasswords += validatePassword(line);
+				countSecurePasswords += validateSecurePassword(line);
             }
-            System.out.println(countValidPasswords);
+			System.out.println("Challenge1: " + countValidPasswords);
+			System.out.println("Challenge2: " + countSecurePasswords);
             bufferedReader.close();         
         }
         catch(FileNotFoundException ex) {
@@ -42,5 +46,20 @@ public class Main {
 			}
         }
         return 1;
-    }
+	}
+	
+	//challenge 2
+	public static int validateSecurePassword(String line){
+		String[] passwords = line.split(" ");
+		// Sort every single password alphabetically
+		for (int i=0; i<passwords.length; i++) {
+			String original = passwords[i];
+			char[] chars = original.toCharArray();
+			Arrays.sort(chars);
+			String sorted = new String(chars);
+			passwords[i] = sorted;
+		}
+
+		return validatePassword(String.join(" ", passwords));
+	}
 }
